87e16d834161bf0df26252179909f851e6b8dcd2
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-29 20:03:16 +0100

    Add project files.

---
+++ AOC9/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AOC9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var sequences = new List<Sequence>();
+            foreach (var line in lines)
+            {
+                var newSequence = new Sequence()
+                {
+                    Content = line.Split(' ').Select(int.Parse).ToList(),
+                };
+                newSequence.GenerateDiffs();
+                newSequence.ExpandSequenceForward();
+                newSequence.ExpandSequenceBackward();
+                sequences.Add(newSequence);
+            }
+
+            Console.WriteLine($"Forward sum (part 1): {sequences.Sum(s => s.Content.Last())}");
+            Console.WriteLine($"Backward sum (part 2): {sequences.Sum(s => s.Content.First())}");
+
+        }
+    }
+}
---
+++ AOC9/Sequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC9
+{
+    internal class Sequence
+    {
+        public List<int> Content { get; set; }
+        public Sequence Diffs { get; set; }
+
+        public void GenerateDiffs()
+        {
+            var diffs = new List<int>();
+            for (int i = 0; i < Content.Count() - 1; i++)
+            {
+                diffs.Add(Content[i + 1] - Content[i]);
+            }
+            Diffs = new Sequence() { Content = diffs };
+            if (!diffs.All(i => i == diffs.First()))
+            {
+                Diffs.GenerateDiffs();
+            }
+        }
+
+        public void ExpandSequenceForward()
+        {
+            if(Diffs == null)
+            {
+                return;
+            }
+
+            Diffs.ExpandSequenceForward();
+
+            Content.Add(Content.Last() + Diffs.Content.Last());
+        }
+
+        public void ExpandSequenceBackward()
+        {
+            if (Diffs == null)
+            {
+                return;
+            }
+
+            Diffs.ExpandSequenceBackward();
+
+            Content.Insert(0, Content.First() - Diffs.Content.First());
+        }
+    }
+}
